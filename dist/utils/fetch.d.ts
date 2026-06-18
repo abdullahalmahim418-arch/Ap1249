11371c5b02bf19a7c2d7c486c43a1a35ea0026c8
Shomik <138145411+SH0MIK@users.noreply.github.com>
2026-06-18 09:27:00 +0600

    Delete dist directory

--- dist/utils/fetch.d.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosInstance } from 'axios';
-export declare function makeClient(baseURL: string, referer: string, extra?: Record<string, string>): AxiosInstance;
-export declare function makeAjaxClient(baseURL: string, referer: string, extra?: Record<string, string>): AxiosInstance;
-export declare const anilistClient: AxiosInstance;
-//# sourceMappingURL=fetch.d.ts.map
